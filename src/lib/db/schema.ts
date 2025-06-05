036faacecf966bcb877f336eb9f50eaa0072c920
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-05 16:55:57 +0300

    Added posts to the schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -49,3 +49,19 @@ export const feed_follows = pgTable(
   },
   (table) => [unique().on(table.user_id, table.feed_id)],
 );
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  title: text("title").notNull(),
+  url: text("url").notNull(),
+  description: text("description").notNull(),
+  published_at: timestamp("published_at"),
+  feed_id: uuid("feed_id")
+    .notNull()
+    .references(() => feeds.id, { onDelete: "cascade" }),
+});
