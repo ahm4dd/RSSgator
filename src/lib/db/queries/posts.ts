03e390aba84856872f3e2e4a15788727d73bdf16
ahm4dd <ahmed.emad.edu@gmail.com>
2025-06-05 17:18:16 +0300

    Added a function to retrieve posts based on the user's id

--- src/lib/db/queries/posts.ts
+++ src/lib/db/queries/posts.ts
@@ -1,11 +1,22 @@
-import { RSSItem } from "../../../RSS/types";
 import { db } from "..";
-import { posts } from "../schema";
-import { getUserByName, getUserById } from "./users";
-import { readConfig } from "../../../config";
-import { Feed, User, NewPost, Post } from "./.././../../lib/db/schema";
-import { sql, asc } from "drizzle-orm";
+import { feeds, posts, users } from "../schema";
+import { NewPost } from "./.././../../lib/db/schema";
+import { sql, getTableColumns } from "drizzle-orm";
+
 export async function createPost(post: NewPost) {
   const [result] = await db.insert(posts).values(post).returning();
+  return result;
+}
+
+export async function getPostsForUser(user_id: string) {
+  const result = db
+    .select({
+      ...getTableColumns(posts),
+      feed_name: feeds.name,
+    })
+    .from(posts)
+    .innerJoin(feeds, sql`${posts.feed_id} = ${feeds.id}`)
+    .innerJoin(users, sql`${feeds.user_id} = ${users.id}`)
+    .where(sql`${users.id} = ${user_id}`);
   return result;
 }
